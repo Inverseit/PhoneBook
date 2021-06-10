7adb2f5c419649b5a59d506239e73e9401c76d35
seitkali_u <useital@andrew.cmu.edu>
2021-06-10 14:15:55 +0600

    basic redis tfa

--- api/src/graphql/user/resolver.js
+++ api/src/graphql/user/resolver.js
@@ -1,8 +1,14 @@
 // const db = require("../../../services/db");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 const DAL = require("./DALuser.js");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
+
+const getRandomCode = () => {
+  const min = 100000;
+  const max = 999999;
+  return Math.floor(Math.random() * (max - min + 1) + min).toString(); //The maximum is inclusive and the minimum is inclusive
+};
 
 // Login resolver (email, password) => AuthInfo
 const login = async (_, { email, password }, context) => {
@@ -11,17 +17,36 @@ const login = async (_, { email, password }, context) => {
     if (res.rows.length != 1) {
       throw new Error("Login credentials error!");
     }
+    const user_id = res.rows[0].user_id;
     const hashInDB = res.rows[0].password;
     const result = await bcrypt.compare(password, hashInDB);
     if (!result) throw new Error("Login credentials error!");
+    // Generate and save a code to the redis
+    const code = getRandomCode();
+    const reply = await context.redis.setAsync(user_id, code);
+    console.log(`${reply} for ${user_id} with code=${code}`);
+    context.redis.client.expire(user_id, 60);
 
-    const user = res.rows[0];
-    const payload = { user_id: user.user_id };
+    return user_id;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const tfa = async (_, { input: { user_id, code } }, context) => {
+  try {
+    // lookup redis and compare
+    const reply = await context.redis.getAsync(user_id);
+    if (reply != code) {
+      throw new Error("Your TFA token is not valid or expired!");
+    }
+    // SUCCESS
+    const payload = { user_id: user_id };
     const token = jwt.sign(payload, process.env.JWT_SECRET, {
       expiresIn: "2h",
     });
     return {
-      user_id: user.user_id,
+      user_id: user_id,
       token: token,
       tokenExpiration: 2,
     };
@@ -60,9 +85,10 @@ const signup = async (
 
 exports.resolver = {
   Query: {
-    login,
+    tfa,
   },
   Mutation: {
     signup,
+    login,
   },
 };
